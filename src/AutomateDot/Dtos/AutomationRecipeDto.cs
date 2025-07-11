1090b11586cf34fae7565065166aa9aa5e5646f6
David Moreira <david_nogueira_moreira@hotmail.com>
2025-07-11 10:43:55 +0100

    Step based insert

--- src/AutomateDot/Dtos/AutomationRecipeDto.cs
+++ src/AutomateDot/Dtos/AutomationRecipeDto.cs
@@ -5,6 +5,95 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AutomateDot.Dtos;
 
+public class AutomationRecipeTriggerDto
+{
+    private TriggerType _triggerType;
+
+    public TriggerType TriggerType
+    {
+        get => _triggerType;
+        set
+        {
+            GitHubWebhookTriggerConfiguration = null;
+            switch (value)
+            {
+                case TriggerType.GitHubWebhook:
+                    GitHubWebhookTriggerConfiguration = new();
+                    break;
+            }
+            _triggerType = value;
+        }
+    }
+
+    [ValidateComplexType]
+    public GitHubWebhookTriggerConfiguration? GitHubWebhookTriggerConfiguration { get; set; } = new();
+
+    public string GetTriggerConfiguration()
+    {
+        switch (TriggerType)
+        {
+            case TriggerType.GitHubWebhook:
+                return System.Text.Json.JsonSerializer.Serialize(GitHubWebhookTriggerConfiguration);
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
+
+public class AutomationRecipeActionDto
+{
+    private ActionType _actionType;
+
+    public ActionType ActionType
+    {
+        get => _actionType;
+        set
+        {
+            SendWebhookConfiguration = null;
+            GotifyConfiguration = null;
+            switch (value)
+            {
+                case Data.Enums.ActionType.SendWebhook:
+                    SendWebhookConfiguration = new();
+                    break;
+
+                case Data.Enums.ActionType.Gotify:
+                    GotifyConfiguration = new();
+                    break;
+            }
+            _actionType = value;
+        }
+    }
+
+    [ValidateComplexType]
+    public SendWebhookConfiguration? SendWebhookConfiguration { get; set; } = new();
+
+    [ValidateComplexType]
+    public GotifyConfiguration? GotifyConfiguration { get; set; }
+
+    public string GetActionConfiguration()
+    {
+        switch (ActionType)
+        {
+            case Data.Enums.ActionType.SendWebhook:
+                return System.Text.Json.JsonSerializer.Serialize(SendWebhookConfiguration);
+
+            case Data.Enums.ActionType.Gotify:
+                return System.Text.Json.JsonSerializer.Serialize(GotifyConfiguration);
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
+
+public class AutomationRecipeDefinitionDto
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+}
+
 public class AutomationRecipeDto
 {
     private TriggerType _triggerType;
